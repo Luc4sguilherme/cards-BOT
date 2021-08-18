bd5244f0a3fa3096c8c07d51f437167d35a2496c
Lucas Guilherme <lucas.guilherme.matutina@gmail.com>
2021-08-18 18:00:12 -0300

    fixed error in loading inventory

--- src/components/isLoadingInventory.js
+++ src/components/isLoadingInventory.js
@@ -5,23 +5,24 @@ export default (msg) => {
     const message = msg.toUpperCase();
 
     if (
-      message === '!SELLALL' ||
-      message === '!SELLCARDS' ||
-      message === '!SELLFOILS' ||
-      message === '!SELLPACKS' ||
-      message === '!STATS' ||
-      message === '!STOCK' ||
-      message === '!WITHDRAWCARDS' ||
-      message === '!WITHDRAWFOILS' ||
-      message === '!WITHDRAWGEMS' ||
-      message === '!WITHDRAWPACKS' ||
-      message === '!WITHDRAWTF' ||
-      message === '!DEPOSITCARDS' ||
-      message === '!DEPOSITFOILS' ||
-      message === '!DEPOSITGEMS' ||
-      message === '!DEPOSITPACKS' ||
-      message === '!DEPOSITTF' ||
-      message === '!RELOAD'
+      message.indexOf('!SELLALL') !== -1 ||
+      message.indexOf('!SELLCARDS') !== -1 ||
+      message.indexOf('!SELLFOILS') !== -1 ||
+      message.indexOf('!SELLPACKS') !== -1 ||
+      message.indexOf('!SELLCHECK') !== -1 ||
+      message.indexOf('!STATS') !== -1 ||
+      message.indexOf('!STOCK') !== -1 ||
+      message.indexOf('!WITHDRAWCARDS') !== -1 ||
+      message.indexOf('!WITHDRAWFOILS') !== -1 ||
+      message.indexOf('!WITHDRAWGEMS') !== -1 ||
+      message.indexOf('!WITHDRAWPACKS') !== -1 ||
+      message.indexOf('!WITHDRAWTF') !== -1 ||
+      message.indexOf('!DEPOSITCARDS') !== -1 ||
+      message.indexOf('!DEPOSITFOILS') !== -1 ||
+      message.indexOf('!DEPOSITGEMS') !== -1 ||
+      message.indexOf('!DEPOSITPACKS') !== -1 ||
+      message.indexOf('!DEPOSITTF') !== -1 ||
+      message.indexOf('!RELOAD') !== -1
     ) {
       return true;
     }
